c1cbe72c36482f5f789f1578d8257e34956b323c
esy <yesman5307@gmail.com>
2022-11-24 04:00:43 +0900

    finished exception Controller

--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,12 +1,18 @@
 package hello.exception.servlet;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -34,8 +40,61 @@ public class ErrorPageController {
         return "error-page/500";
     }
 
+    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPage500Api(
+            HttpServletRequest request, HttpServletResponse response) {
+
+        log.info("API errorPage 500");
+
+        Map<String, Object> result = new HashMap<>();
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message", ex.getMessage());
+
+        Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+//        Enumeration<String> attributeNames = request.getAttributeNames();
+//        attributeNames.asIterator().forEachRemaining(System.out::println);
+
+/*
+        javax.servlet.forward.request_uri
+        javax.servlet.forward.context_path
+        javax.servlet.forward.servlet_path
+        javax.servlet.forward.mapping
+        org.springframework.web.servlet.View.pathVariables
+        org.springframework.web.context.request.async.WebAsyncManager.WEB_ASYNC_MANAGER
+        org.springframework.web.servlet.HandlerMapping.bestMatchingHandler
+        javax.servlet.error.status_code
+        org.springframework.web.servlet.DispatcherServlet.CONTEXT
+        org.springframework.web.servlet.resource.ResourceUrlProvider
+        org.springframework.web.util.ServletRequestPathUtils.PATH
+        org.springframework.web.servlet.DispatcherServlet.LOCALE_RESOLVER
+        org.springframework.web.servlet.HandlerMapping.bestMatchingPattern
+        requestContextFilter.FILTERED
+        org.springframework.web.servlet.DispatcherServlet.OUTPUT_FLASH_MAP
+        javax.servlet.error.exception_type
+        logId
+        org.springframework.web.servlet.DispatcherServlet.FLASH_MAP_MANAGER
+        org.springframework.core.convert.ConversionService
+        org.springframework.web.servlet.HandlerMapping.matrixVariables
+        org.springframework.boot.web.servlet.error.DefaultErrorAttributes.ERROR
+        javax.servlet.error.message
+        javax.servlet.error.servlet_name
+        org.springframework.web.servlet.DispatcherServlet.THEME_SOURCE
+        org.springframework.web.servlet.HandlerMapping.producibleMediaTypes
+        org.springframework.web.servlet.HandlerMapping.pathWithinHandlerMapping
+        org.springframework.web.servlet.HandlerMapping.uriTemplateVariables
+        javax.servlet.error.request_uri
+        org.springframework.web.servlet.DispatcherServlet.THEME_RESOLVER
+        javax.servlet.error.exception
+*/
+        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
+    }
+
+
     private void printErrorInfo(HttpServletRequest request) {
         log.info("ERROR_EXCEPTION: {}", request.getAttribute(ERROR_EXCEPTION));
+//        log.info("ERROR_EXCEPTION: ex= {}", request.getAttribute(ERROR_EXCEPTION));
         log.info("ERROR_EXCEPTION_TYPE: {}", request.getAttribute(ERROR_EXCEPTION_TYPE));
         log.info("ERROR_MESSAGE: {}", request.getAttribute(ERROR_MESSAGE));
         log.info("ERROR_REQUEST_URI: {}", request.getAttribute(ERROR_REQUEST_URI));
